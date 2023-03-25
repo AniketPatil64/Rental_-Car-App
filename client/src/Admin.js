e15fe58d57a34e99707eb01b196db5dae7731919
Muniswamy Maladasari <muniswamyanil143@gmail.com>
2023-03-25 21:56:28 +0530

    carbooking page created by p2

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -3,6 +3,7 @@ import AdminPage from "./components/Adminpage/AdminPage"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddCar from './components/AddCar/AddCar';
 import EditCar from './components/EditCar/EditCar';
+import CarBooking from './components/Carbookings/CarBooking';
 
 function Admin() {
   return (
@@ -11,6 +12,7 @@ function Admin() {
       <Route path='/adminPage' element={<AdminPage/>}/>
       <Route path='/addcar' element={<AddCar/>}/>
       <Route path='/editcar' element={<EditCar/>}/>
+      <Route path='/bookingcar' element={<CarBooking/>}/>
     </Routes>
   </BrowserRouter>
   )
