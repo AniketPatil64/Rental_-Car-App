37ae0709fb96a809c773c9206b59d25606680348
Sourabh Dudhale <sourabhdudhale@gmail.com>
2023-03-25 23:42:40 +0530

    login part frontend created by p3

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import Admin from './Admin';
+//import Admin from './Admin';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import AdminLogin from './components/AdminLogin/AdminLogin';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
-  <Admin/>
+    // <Admin/>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/adminlogin' element={<AdminLogin />} />
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/AdminLogin/AdminLogin.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function AdminLogin() {
+    const navigate = useNavigate();
+    const [emailOrPhone, setEmailOrPhone] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+  return (
+    <div className='login flex flex-col'>
+            <div className='left '>
+            <div className='image'>
+
+                <div className='line'>
+                    <h1>All you needed was a wheel in Your hand and four on the road.</h1>
+                </div>
+
+                <div className='routers'>
+                    <span className='register' onClick={() => navigate('/')}>
+                        User sign in
+                    </span>
+                    
+                </div>
+                </div>
+                <div className='right'>
+                <div className='loginform'>
+                    <form onSubmit={handleSubmit} className="login-form">
+                        <h5>Sign in your Admin Account</h5>
+                        <input
+                            type="text"
+                            id='email'
+                            value={emailOrPhone}
+                            onChange={(event) => setEmailOrPhone(event.target.value)}
+                            placeholder='Phone Number or Email'
+                        />
+                        <br />
+                        <input
+                            type="password"
+                            id='password'
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                            placeholder='Password'
+                        />
+                        <br/>
+                        <a href="/" className="forgot-password-link">
+                            Forgot Password?
+                        </a>
+                        <button type="submit">Log In</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+                </div>
+  )
+}
+
+export default AdminLogin
